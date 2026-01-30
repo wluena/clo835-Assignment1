61d0980c18a3c1ec6085f7b362cc84513cf2b2ab
wluena <winlyntiuluena@gmail.com>
2026-01-30 05:10:25 +0000

    Final deployment4

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template #Final Deployment3
+from flask import Flask, render_template #Final Deployment4
 import pymysql
 import os
 template_dir = os.path.abspath('templates')
